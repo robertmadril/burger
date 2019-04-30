50d3b1d1a1afe01dfc2c3bc12947b16a2639e8ba
robertmadril <robertmmadril@gmail.com>
2019-04-29 20:10:29 -0700

    add db schema

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+CREATE TABLE burgers (
+
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL
+
+);
+
+
